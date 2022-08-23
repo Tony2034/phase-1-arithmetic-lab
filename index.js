79968b0035857072cb7a98ca125ac388e5b22a06
Tonny Blair Okumu <hellotony2034@gmail.com>
2022-08-23 11:53:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 62
+let num2 = 1
+let multiply = num1 * num2
+
+let random = Number.isInteger(Math.random() * 10) + 1
+
+let num3 = 44
+let num4 = 10
+let mod = 44 % 10
+
+let max = Math.max(5, 10, 15, 20)
